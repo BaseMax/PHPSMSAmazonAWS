1b7d0d3574921862154922ed34a6a8d6d003c28c
Max Base <MaxBaseCode@Gmail.Com>
2020-09-20 15:10:00 +0430

    Adding copyright/header

--- amazon-aws-sms.php
+++ amazon-aws-sms.php
@@ -1,1 +1,8 @@
-
+<?php
+/*
+ * @Name: PHP SMS Amazon SNS AWS
+ * @Repository: https://github.com/BaseMax/PHPSMSAmazonAWS
+ * @Description: A quick example of sending SMS with AWS SNS Using Amazon Service. 
+ * @Author: Max Base
+ * @Date: 2020-09-20
+ */
