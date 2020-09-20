5c085b3bcdfd051dca3cd6fc15fa0ad44eeb5ca0
Max Base <MaxBaseCode@Gmail.Com>
2020-09-20 15:10:54 +0430

    Adding key/secret

--- amazon-aws-sms.php
+++ amazon-aws-sms.php
@@ -6,3 +6,14 @@
  * @Author: Max Base
  * @Date: 2020-09-20
  */
+require './vendor/autoload.php';
+$KEY='';
+$SECRET='';
+$parameters = [
+    'version' => 'latest',
+    'region' => 'us-east-1',
+    'credentials' => [
+        'secret' => $SECRET,
+        'key' => $KEY,
+    ],
+];
